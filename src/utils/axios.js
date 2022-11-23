b833fde7fc970073546244745e0686b6f347b5e9
Ahsan Amin <ahsan.amin.eu181400095@gmail.com>
2022-11-24 01:12:41 +0600

    NewFolder page width updated

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     // baseURL: "http://localhost:9000"
-    baseURL: `https://directorymernbackend-production.up.railway.app/`
+    baseURL: `https://directorymernbackend-production.up.railway.app`
 })
 export default axiosInstance; 
